2d56133e806f5168c794258e1f831d2f50ebaa87
Junhyuk Noh <njh2239@gmail.com>
2024-05-25 19:41:49 +0900

    first commit

---
+++ src/main/kotlin/com/njhyuk/codi/CodiApplication.kt
@@ -0,0 +1,11 @@
+package com.njhyuk.codi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodiApplication>(*args)
+}
---
+++ src/test/kotlin/com/njhyuk/codi/CodiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.njhyuk.codi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
